982065a10c80c1ef49a88782fce0330996af5fe2
Togashi-sys <t.togashi@strategit.jp>
2022-06-26 12:21:10 +0900

    ♻ pycodestyleにリファクタリング

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
 import config
 from organizedFile import file
 
+
 def main():
-  file.organizedFiles(config.BRAVE_PATH, config.AFTER_ORGANIZE_PATH)
-  file.organizedFiles(config.SLACK_PATH, config.AFTER_ORGANIZE_PATH)
+    file.organizedFiles(config.BRAVE_PATH, config.AFTER_ORGANIZE_PATH)
+    # file.organizedFiles(config.SLACK_PATH, config.AFTER_ORGANIZE_PATH)
+    # file.organizedFiles(config.SCREENSHOTS_PATH, config.SCREENSHOTS_PATH)
+
 
 if __name__ == '__main__':
-  main()
+    main()
